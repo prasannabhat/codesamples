9eb6041e6e8ec348e2cc459317b49a97fa28ab73
prasannabhat <prasanna.yoga@gmail.com>
2012-02-09 01:52:17 +0530

    Add rexml tutorial

---
+++ ruby/xml/rexmltuts/xmlparse.rb
@@ -0,0 +1,38 @@
+
+#!/usr/bin/ruby -w
+
+require 'rexml/document'
+#gem install awesome_print
+require 'ap'
+require "pp"
+include REXML
+
+doc = Document.new File.new("mydoc.xml")
+doc.elements.each("inventory/section") { |element| puts element.attributes["name"] }
+# -> health
+# -> food
+doc.elements.each("//section/item") { |element| puts element.attributes["upc"] }
+# -> 123456789
+# -> 445322344
+# -> 485672034
+# -> 132957764
+root = doc.root
+puts root.attributes["title"]
+# -> OmniCorp Store #45x10^3
+puts root.elements["section/item[@stock='44']"].attributes["upc"]
+# -> 132957764
+puts root.elements["section"].attributes["name"] 
+# -> health (returns the first encountered matching element) 
+puts root.elements[1].attributes["name"] 
+# -> health (returns the FIRST child element) 
+root.detect {|node| node.kind_of? Element and node.attributes["name"] == "food" }
+
+
+# Using XPath
+# The invisibility cream is the first <item>
+invisibility = XPath.first( doc, "//item" ) 
+# Prints out all of the prices
+XPath.each( doc, "//price") { |element| puts element.text }
+# Gets an array of all of the "name" elements in the document.
+names = XPath.match( doc, "//name" )
+p names
---
+++ ruby/xml/xmlxpath.rb
@@ -0,0 +1,20 @@
+
+#!/usr/bin/ruby -w
+
+require 'rexml/document'
+include REXML
+
+xmlfile = File.new("movies.xml")
+xmldoc = Document.new(xmlfile)
+
+# Info for the first movie found
+movie = XPath.first(xmldoc, "//movie")
+p movie
+
+# Print out all the movie types
+XPath.each(xmldoc, "//type") { |e| puts e.text }
+
+# Get an array of all of the movie formats.
+names = XPath.match(xmldoc, "//format").map {|x| x.text }
+# names = XPath.match(xmldoc, "//format")
+p names
