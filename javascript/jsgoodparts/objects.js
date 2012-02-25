e67fa15b74b13131aa94f1d9674747837d893906
prasannabhat <prasanna.yoga@gmail.com>
2012-02-25 13:29:26 +0530

    code example for delete operator.

--- javascript/jsgoodparts/objects.js
+++ javascript/jsgoodparts/objects.js
@@ -40,6 +40,8 @@ flight.writeToDoc();
 
 //Prototype
 var another_stooge = Object.beget(stooge);
+another_stooge['first-name'] = "Prasanna";
+another_stooge.nickname = 'Bhatru';
 writeToDoc("\nanother_stooge is : ");
 another_stooge.writeToDoc();
 
@@ -49,6 +51,12 @@ another_stooge.profession    // 'actor'
 
 flight.hasOwnProperty('number')         // true
 flight.hasOwnProperty('constructor')    // false
+
+// The delete operator can be used to remove a property from an object. It will remove a property from the object if it has one. It will not touch any of the objects in the prototype linkage.
+// Removing a property from an object may allow a property from the prototype linkage to shine through:
+delete another_stooge['first-name'];
+writeToDoc("\nanother_stooge after delete operation");
+another_stooge.writeToDoc();
 	
 // });
 
