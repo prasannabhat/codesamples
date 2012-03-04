5b9ee8aa963c7e10098b2582b4134c0811cb132f
prasannabhat <prasanna.yoga@gmail.com>
2012-03-04 12:07:11 +0530

    Changes to ruby-git examples

--- ruby/git/ruby-git.rb
+++ ruby/git/ruby-git.rb
@@ -3,13 +3,29 @@ require 'git'
 require 'ap'
 require 'pp'
 
-repo = "D:/gitrepos/Development/Dev_ComStack_Ar40"
+
+repo = "E:/gitrepos/Testing/jquery-mobile"
+remo = "E:/gitrepos/Testing/bare"
 g = Git.open repo
 
 #puts g.log
 
-branches = g.branches[:b_integration]
-puts branches
+branches = g.branches
+# puts g.command 'fsck'
+puts g.command 'branch'
+# puts g.command 'tag'
+remotes = g.remotes
+remote = g.remote 'origin'
+puts "Remote repository is #{remote.url}"
+r = Git.open remo
+puts r
+
+# g.command 'gc'
+
+# remotes = g.remotes
+# puts remotes
+
+# g.fsck
 #pp branches
 #ap branches
 
